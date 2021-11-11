05fbd860135d77ba09e9630bd3cc9af35457523f
Beatriz <beatrizsa1906@gmail.com>
2021-11-11 01:25:13 +0100

    Project

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+
+    public int damage = 1;
+    public float speed;
+    public int obstacleHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+        CheckHealth();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")){
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+
+        if (other.CompareTag("Fireball"))
+        {
+            Destroy(other.gameObject);
+            obstacleHealth-=5;
+        }
+
+        if (other.CompareTag("Crystal"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void CheckHealth()
+    {
+        if(obstacleHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed;
+    public float minHeight;
+    public float maxHeight;
+
+    public int health = 3;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        
+        float speedValue = speed * Time.deltaTime;
+ 
+        Vector2 newYPos = new Vector2(0, cursorPos.y);   // Disregard the player's x and z position.
+
+        if(transform.position.y >= minHeight && transform.position.y <= maxHeight){
+            transform.position = Vector2.MoveTowards (transform.position, newYPos, speedValue);
+        }
+        else if(transform.position.y < minHeight){
+            transform.position = new Vector2(transform.position.x, minHeight);
+        }
+        else if(transform.position.y > maxHeight){
+            transform.position = new Vector2(transform.position.x, maxHeight);
+        }       
+        
+    }
+}
